c186d118794f12792a23c077d0b7ddb9c11e355a
WIFIxFTT <peter4263676@gmail.com>
2015-08-20 16:42:47 +0800

    820
    homework4

--- js/blog.countnum.js
+++ js/blog.countnum.js
@@ -1,16 +1,24 @@
-var answer = "1234";
-var guess = "2335";
-var A = 0;
-var B = 0;
+var Games = function(){
+	var answer = prompt("Put in your answers.");
+            while(true){
+              var guess = prompt("Let's guess!")
+              var number = 0;
+              var A = 0;
+              var B = 0;
 
-for (var i = 0; i < answer.length; i++) {
-	for (var j = 0; j < guess.length; j++){
-		if (answer[i] == guess[j] && i == j )
-			A++;
-		if(answer[i] == guess[j])
-			B++;
-	};
-};
-
-console.log(A + B);
-	
+              number++;
+              for (var i = 0; i < answer.length; i++) {
+                for (var j = 0; j < guess.length; j++){
+                  if (answer[i] === guess[j] && i === j)
+                    A++;
+                  if(answer[i] === guess[j] && i !== j)
+                    B++;
+                }
+              }
+              document.write(A+"A"+B+"B<br>");
+              if(A === 4){
+                document.write("congratulations!<br> You guess "+ number +" times.");
+                break;
+              }
+            }
+    }
